f55e390a4df8076348ca0bea27bee8ec07febb05
George Chondromatidis <giorgos.cho@hotmail.com>
2023-10-30 16:27:21 +0200

    Change project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishItem';
+
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete,
+]
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
-  template: `<h1>Default</h1>`,
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'default';
+  items : WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Go to Super Market')
+  ];
+  listFilter : string = '0';
+  newWishText = '';
+  title = 'angular-tasklist';
+  
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[parseInt(this.listFilter)]);
+  }
+
+  addNewWish(){
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  toggleItem(item: any){
+    item.isComplete = !item.isComplete;
+  }
+
 }
