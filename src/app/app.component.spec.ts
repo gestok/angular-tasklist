86b6a764ce5d1354c86ae67603a5185c010e11da
George Chondromatidis <giorgos.cho@hotmail.com>
2023-11-06 16:26:45 +0200

    Wish > Task

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-wishlist'`, () => {
+  it(`should have as title 'angular-tasklist'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
   });
@@ -23,8 +23,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'angular-wishlist app is running!'
-    );
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-tasklist app is running!');
   });
 });
